8b4c6e38a9b63857f5a00c941812fb41eb136bef
Sua Lee (Hannah) <peter48@inu.ac.kr>
2025-12-23 22:26:19 +0900

    feat: 자기소개란 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
+      <main className="flex min-h-screen flex-col items-center justify-between py-32 px-16">
+        <div className="flex flex-col items-center py-32 px-32 space-y-6 text-center">
+          <h1 className="text-5xl">Hi, I'm Sua Lee – Web Developer in South Korea</h1>
+          <h1 className="text-3xl mx-3">Computer Science Student & Web Developer</h1>
+          <h1 className="text-base mx-10 mb-10">I am a Computer Science student at Incheon National University,<br/>
+            building web applications with a focus on clarity, structure, and usability.<br></br>
+            I mainly work with Next.js and React and enjoy turning ideas into well-organized, 
+            functional interfaces.
+          </h1>
+        </div>
+      </main>
     </div>
   );
 }
