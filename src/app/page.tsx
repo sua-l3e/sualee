4e7d5e5016e92195d256653b017c06a6fd5f5029
Sua Lee (Hannah) <peter48@inu.ac.kr>
2025-12-30 20:52:05 +0900

    tag refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between py-32 px-16">
         <div className="flex flex-col items-center py-32 px-32 space-y-6 text-center">
           <h1 className="text-5xl">Hi, I'm Sua Lee – Web Developer in South Korea</h1>
-          <h1 className="text-3xl mx-3">Computer Science Student & Web Developer</h1>
-          <h1 className="text-base mx-10 mb-10">I am a Computer Science student at Incheon National University,<br/>
+          <h2 className="text-3xl mx-3">Computer Science Student & Web Developer</h2>
+          <p className="text-base mx-10 mb-10">I am a Computer Science student at Incheon National University,<br/>
             building web applications with a focus on clarity, structure, and usability.<br></br>
             I mainly work with Next.js and React and enjoy turning ideas into well-organized, 
             functional interfaces.
-          </h1>
+          </p>
         </div>
       </main>
     </div>
